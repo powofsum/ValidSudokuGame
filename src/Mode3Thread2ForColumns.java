aa1466fb69df48a5a70f4fd50e7631b084b158aa
Samar <sososa02004@gmail.com>
2025-11-29 01:36:21 +0200

    mode3

---
+++ src/Mode3Thread2ForColumns.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Mode3Thread2ForColumns implements Runnable{
+
+
+    private final String path = "test.csv";
+    IModesValidate validate=new Validation();
+    SudokuGrid gridLoader = new SudokuGrid(path);
+    int[][] grid = gridLoader.loadFile(path);
+    private  boolean anyError=false;
+    ArrayList<String> Message= new ArrayList<>();
+
+
+
+    @Override
+    public void run(){
+        checkColumns();
+    }
+
+    private void checkColumns(){
+        for (int i = 1; i <= 9; i++) {
+            int[] col = gridLoader.getColumn(grid, i);
+            ArrayList<Integer> duplicate = validate.checkUnit(col);
+            if (!duplicate.isEmpty()) {
+                anyError=true;
+                for (int d : duplicate) {
+
+                    String s="COl " + i + ", #" + d + ", " + FormatArray.formatArray(col);
+                    Message.add(s);
+                }
+            }
+        }
+
+    }
+
+
+    public boolean foundAnyError() {
+        return anyError;
+    }
+    public ArrayList<String> getMessage() {
+        return Message;
+    }
+}
---
+++ src/Mode3Thread3ForBoxes.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Mode3Thread3ForBoxes implements Runnable {
+    IModesValidate validate=new Validation();
+    private final String path = "test.csv";
+    SudokuGrid gridLoader = new SudokuGrid(path);
+    int[][] grid = gridLoader.loadFile(path);
+    private boolean anyError=false;
+    ArrayList<String> Message= new ArrayList<>();
+
+    @Override
+    public void run(){
+        checkBoxes();
+    }
+
+    private void checkBoxes(){
+        for (int i = 1; i <= 9; i++) {
+            int[] box = gridLoader.getBox(grid, i);
+            ArrayList<Integer> duplicate = validate.checkUnit(box);
+
+            if (!duplicate.isEmpty()) {
+                anyError=true;
+                for (int d : duplicate) {
+
+                    String s="BOX " + i + ", #" + d + ", " + FormatArray.formatArray(box);
+                    Message.add(s);
+                }
+            }
+
+        }
+
+    }
+
+    public boolean foundAnyError() {
+        return anyError;
+    }
+    public ArrayList<String> getMessage() {
+        return Message;
+    }
+}
