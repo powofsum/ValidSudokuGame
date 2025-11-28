3f5490f367da863a4c459af047b02fbf47e00ac4
Samar <sososa02004@gmail.com>
2025-11-29 01:38:43 +0200

    sout

---
+++ src/OutputForModes.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public  class OutputForModes  {  //sout
+
+    public static void  outputMessage(ArrayList<String > Message){
+        for(String s:Message){
+            System.out.println(s);
+        }
+        System.out.println("-------------------------\n");
+
+    }
+}
